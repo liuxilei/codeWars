892806065e94f8629c5d0176cb9e48f79f40d608
liuxilei <liuxileioo@qq.com>
2019-01-03 23:17:20 +0800

    doc: Reverse polish notation calculator

--- 0014.js
+++ 0014.js
@@ -23,19 +23,4 @@ function calc(expr) {
 
 console.log(calc('5 1 2 + 4 * + 3 -'));
 
-function calc(expr) {
-    var result = [];
-    var atoms = expr.split(/\s+/);
-    var operators = ['+', '-', '*', '/'];
-    for (var i = 0; i < atoms.length; i++) {
-        switch (atoms[i]) {
-            case '+': result.push(result.pop() + result.pop()); break;
-            case '-': result.push(-result.pop() + result.pop()); break;
-            case '*': result.push(result.pop() * result.pop()); break;
-            case '/': result.push(1 / (result.pop() / result.pop())); break;
-            default: result.push(parseFloat(atoms[i]));
-        }
-    }
-    return result.pop() || 0;
-}
 
