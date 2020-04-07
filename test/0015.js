9a2ea935c320457761df27de663d4139ba8f5b84
liuxilei <liuxileioo@qq.com>
2020-04-07 14:22:23 +0800

    feat(0015): 截取一段url的域名部分
    截取任意url字符串的域名部分,自己未想出解决办法参考的solution

---
+++ test/0015.js
@@ -0,0 +1,11 @@
+function domainName(url) {
+    return url.replace("https://", "")
+        .replace("http://", "")
+        .replace("www.", "")
+        .split(".")[0]
+}
+
+console.log(domainName("http://google.com") === "google");
+console.log(domainName("http://google.co.jp") === "google");
+console.log(domainName("www.xakep.ru") === "xakep");
+console.log(domainName("https://youtube.com") === "youtube");
